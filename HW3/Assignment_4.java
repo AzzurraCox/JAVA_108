59f5df6897a99781289eb3e60559875ba026c517
AzzurraCox <39265378+AzzurraCox@users.noreply.github.com>
2020-01-01 00:59:49 +0800

    Update Assignment_4.java

--- HW3/Assignment_4.java
+++ HW3/Assignment_4.java
@@ -1,9 +1,4 @@
-/** 
- * Assignment #: 4
- * Name: §d¸t°¶
- * StudentID: 405530011
- * Description: Call the method in Preprocessing class.
-*/
+
 public class Assignment_4 {
 
 	public static void main(String[] args) {
